f91a8ad8bd71ff2f5040bca70f440269d910347c
Fray León Osorio Rivera <frayosorio@gmail.com>
2023-11-16 21:26:58 -0500

    Se agrega controlador para EMPRESAS

---
+++ src/main/java/apitiendavideo/apitiendavideo/controladores/EmpresaControlador.java
@@ -0,0 +1,10 @@
+package apitiendavideo.apitiendavideo.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/empresas")
+public class EmpresaControlador {
+    
+}
