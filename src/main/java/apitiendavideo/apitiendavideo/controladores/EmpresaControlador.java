2873c3eadf878ebbf8fe9e8545450d0eef513d53
Fray León Osorio Rivera <frayosorio@gmail.com>
2023-11-24 21:59:00 -0500

    Habilitacion de referencias cruzadas

--- src/main/java/apitiendavideo/apitiendavideo/controladores/EmpresaControlador.java
+++ src/main/java/apitiendavideo/apitiendavideo/controladores/EmpresaControlador.java
@@ -2,6 +2,7 @@ package apitiendavideo.apitiendavideo.controladores;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,34 +15,39 @@ import apitiendavideo.apitiendavideo.interfaces.IEmpresaServicio;
 @RestController
 @RequestMapping("/empresas")
 public class EmpresaControlador {
-    
+
     private IEmpresaServicio servicio;
 
     public EmpresaControlador(IEmpresaServicio servicio) {
         this.servicio = servicio;
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public List<Empresa> listar() {
         return this.servicio.listar();
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/obtener/{id}", method = RequestMethod.GET)
     public Empresa obtener(@PathVariable long id) {
         return this.servicio.obtener(id);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/buscar/{texto}", method = RequestMethod.GET)
     public List<Empresa> obtener(@PathVariable String texto) {
         return this.servicio.buscar(texto);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/agregar", method = RequestMethod.POST)
     public Empresa agregar(@RequestBody Empresa empresa) {
         empresa.setId(0);
         return this.servicio.guardar(empresa);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/modificar", method = RequestMethod.PUT)
     public Empresa modificar(@RequestBody Empresa empresa) {
         if (this.servicio.obtener(empresa.getId()) != null) {
@@ -51,6 +57,7 @@ public class EmpresaControlador {
         }
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/eliminar/{id}", method = RequestMethod.DELETE)
     public boolean eliminar(@PathVariable long id) {
         return this.servicio.eliminar(id);
--- src/main/java/apitiendavideo/apitiendavideo/controladores/PaisControlador.java
+++ src/main/java/apitiendavideo/apitiendavideo/controladores/PaisControlador.java
@@ -2,6 +2,7 @@ package apitiendavideo.apitiendavideo.controladores;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,27 +22,32 @@ public class PaisControlador {
         this.servicio = servicio;
     }
 
+    @CrossOrigin(origins="*")
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public List<Pais> listar() {
         return this.servicio.listar();
     }
 
+    @CrossOrigin(origins="*")
     @RequestMapping(value = "/obtener/{id}", method = RequestMethod.GET)
     public Pais obtener(@PathVariable long id) {
         return this.servicio.obtener(id);
     }
 
+    @CrossOrigin(origins="*")
     @RequestMapping(value = "/buscar/{texto}", method = RequestMethod.GET)
     public List<Pais> obtener(@PathVariable String texto) {
         return this.servicio.buscar(texto);
     }
 
+    @CrossOrigin(origins="*")
     @RequestMapping(value = "/agregar", method = RequestMethod.POST)
     public Pais agregar(@RequestBody Pais pais) {
         pais.setId(0);
         return this.servicio.guardar(pais);
     }
 
+    @CrossOrigin(origins="*")
     @RequestMapping(value = "/modificar", method = RequestMethod.PUT)
     public Pais modificar(@RequestBody Pais pais) {
         if (this.servicio.obtener(pais.getId()) != null) {
@@ -51,6 +57,7 @@ public class PaisControlador {
         }
     }
 
+    @CrossOrigin(origins="*")
     @RequestMapping(value = "/eliminar/{id}", method = RequestMethod.DELETE)
     public boolean eliminar(@PathVariable long id) {
         return this.servicio.eliminar(id);
